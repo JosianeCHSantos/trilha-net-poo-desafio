e17aa73b0064cb6bb8c6a4e6a9eb80c0499f939b
Josiane Cristina Henrique dos Santos <jacphs1326@outlook.com>
2022-11-04 00:14:13 -0300

    Desafio Concluídogit status!

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "99997777", modelo: "Modelo A", imei: "00010007", memoria: 128 );
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone iPhone: ");
+Smartphone iphone = new Iphone(numero: "99000107", modelo: "Modelo B", imei: "00020007", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Telegram");
